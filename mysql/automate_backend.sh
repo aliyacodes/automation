fe855a2245efdca8dd7bf62cd70bef8123e9f2db
Aliya <22922673+aliyacodes@users.noreply.github.com>
2021-04-28 15:20:21 -0700

    escape single quotes

--- mysql/automate_backend.sh
+++ mysql/automate_backend.sh
@@ -14,8 +14,8 @@ CREATE TABLE info (
     witticism varchar(1000), 
     sarcastic_gloat varchar(1000)
 );
-GRANT USAGE ON *.* TO 'MyApp'@'%' IDENTIFIED BY PASSWORD '*236023AE71553ECD5E234575E2EEC41C32263119';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `back_end`.* TO 'MyApp'@'%';" >> file
+GRANT USAGE ON *.* TO \'MyApp\'@\'%\' IDENTIFIED BY PASSWORD \'*236023AE71553ECD5E234575E2EEC41C32263119\';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `back_end`.* TO \'MyApp\'@\'%\';" >> file
 
 mysql < file
 
